470874a1d167493b3e6c2cbb60a811dfdc7f32ef
LewisJones30 <43572935+LewisJones30@users.noreply.github.com>
2019-11-05 15:11:38 +0000

    Addition of Towers of Hanoi assets and prefabs, font added too
    New content:
    Addition of new prefabs for:
    Towers of Hanoi states 0 rings, 1 ring, 2 ring, 3 ring, 4 ring.
    Seven-Segment Font (Free for personal use: https://www.dafont.com/seven-segment.font)
    Bug fixes:
    None

---
+++ Escape Room/Assets/Scripts/Timer.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+/*
+ * Adapted from AINT254 Timer Script.
+ */
+public class Timer : MonoBehaviour
+{
+    public float timeLeft = 60.0f;
+    public Text timeLeftText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeLeftText.color = Color.green;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        timeLeft -= Time.deltaTime; //This is used as the time remaining counter.
+        int minutes = Mathf.FloorToInt(timeLeft / 60f);
+        int seconds = Mathf.FloorToInt(timeLeft - minutes * 60);
+        string timeLeftString = string.Format("{0:00}:{1:00}", minutes, seconds);
+        timeLeftText.text = timeLeftString;
+      
+        if (timeLeft < 300) //5 Minutes remaining
+        {
+            timeLeftText.color = Color.yellow;
+            if (timeLeft < 60) // 1 Minute remaining
+            {
+                timeLeftText.color = Color.red;
+                if (timeLeft < 30)
+                {
+                    if (seconds % 2 == 0)
+                    {
+                        timeLeftText.color = Color.white;
+                    }
+                }
+            }
+        }
+        if (timeLeft < 0)
+        {
+            //Code when time ends to start explosion and player fainting sequence.
+            //SceneManager.LoadScene("GameLoseScene");
+        }
+
+    }
+}
