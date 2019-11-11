a1431afa91fee3c10d2661953d07441bde1cd3df
LewisJones30 <43572935+LewisJones30@users.noreply.github.com>
2019-11-11 16:25:23 +0000

    Addition of raycasting script
    Additions:
    Raycasting script added to player (main camera). This script will be used to mainly interact with objects such as buttons. Currently bugged, pushing to Github to have a backup of the original script.
    Invisible object where the towers are, to make the raycast have a larger object to hit.
    Bug fixes/Updates:
    None.

---
+++ Escape Room/Assets/Scripts/MovementScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+
+    Rigidbody player;
+    public float movementSpeed = 15; //Multiplier for movement speed
+    public float rotationSpeed = 10; //Multiplier for rotation speed
+    public Rigidbody rb;
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddRelativeForce(new Vector3(0.0f, 0.0f, movementSpeed * - 1));
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(new Vector3(0.0f, (Input.GetAxis("Horizontal") * rotationSpeed * Time.fixedDeltaTime), 0.0f));
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddRelativeForce(new Vector3(0.0f, 0.0f, movementSpeed));
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(new Vector3(0.0f, (Input.GetAxis("Horizontal") * rotationSpeed * Time.fixedDeltaTime), 0.0f));
+        }
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Rotate(new Vector3((Input.GetAxis("Vertical")* rotationSpeed * Time.fixedDeltaTime), 0.0f, 0.0f));
+        }
+    }
+}
+
---
+++ Escape Room/Assets/Scripts/RaycastPlayer.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RaycastPlayer : MonoBehaviour
+{
+    RaycastHit hit;
+    Ray raycast;
+    public Text towersInteract;
+
+    void Start()
+    {
+        towersInteract.enabled = false;
+    }
+
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 forward = transform.TransformDirection(Vector3.forward) * 2.5f;
+        Debug.DrawRay(transform.position, forward, Color.red);
+        if (Physics.Raycast(transform.position, (forward), out hit))
+        {
+            if (hit.collider.gameObject.name == "puzzle1_trigger")
+            {
+                if (towersInteract.enabled == false)
+                {
+                    towersInteract.enabled = true;
+                }
+                //Transfer player to the towers of hanoi puzzle scene - don't forget to pass puzzle variables if not done already!
+                if (Input.GetKey(KeyCode.E))
+                {
+                    Debug.Log("E pressed!");
+                }
+            }
+        }
+        else
+        {
+            if (towersInteract.enabled == true)
+            {
+                towersInteract.enabled = false;
+            }
+        }
+    }
+}
