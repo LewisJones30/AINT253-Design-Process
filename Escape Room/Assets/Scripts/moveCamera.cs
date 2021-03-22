83ddbe738654016b5bb6bda182cfe619872b9364
LewisJones30 <43572935+LewisJones30@users.noreply.github.com>
2021-03-22 15:38:34 +0000

    Version 1.0.1
    Fixes an issue with the camera.

--- Escape Room/Assets/Scripts/moveCamera.cs
+++ Escape Room/Assets/Scripts/moveCamera.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 public class moveCamera : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    [SerializeField]
+    float sensitivityX;
+    [SerializeField]
+    float sensitivityY;
+
+    float rotationX = 0f;
+    float rotationY = 0f;
     void Start()
     {
         
@@ -13,8 +21,9 @@ public class moveCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float h = Input.GetAxis("Mouse X");
-        float v = Input.GetAxis("Mouse Y");
-        transform.Rotate(v, h, 0);
+        rotationX += Input.GetAxis("Mouse X") * sensitivityX;
+        rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+        rotationY = Mathf.Clamp(rotationY, -90, 90);
+        transform.eulerAngles = new Vector3(-rotationY, rotationX, 0);
     }
 }
